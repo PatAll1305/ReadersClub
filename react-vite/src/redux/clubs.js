0cda909966a546c4877c0e976ea5c1d277dceff3
Patrick Allen <patrickallen1305@gmail.com>
2025-01-01 18:53:19 -0600

    Completed redux store for club messages

--- react-vite/src/redux/clubs.js
+++ react-vite/src/redux/clubs.js
@@ -4,6 +4,10 @@ const LOAD_CLUBS = 'clubs/LOAD_CLUBS';
 const ADD_CLUB = 'clubs/ADD_CLUB';
 const JOIN_CLUB = 'clubs/JOIN_CLUB';
 const LEAVE_CLUB = 'clubs/LEAVE_CLUB';
+const LOAD_CLUB_MESSAGES = 'clubs/LOAD_CLUB_MESSAGES';
+const ADD_CLUB_MESSAGE = 'clubs/ADD_CLUB_MESSAGE';
+const DELETE_CLUB_MESSAGE = 'clubs/DELETE_CLUB_MESSAGE';
+const EDIT_CLUB_MESSAGE = 'clubs/EDIT_CLUB_MESSAGE';
 
 const loadClubs = (clubs) => ({
     type: LOAD_CLUBS,
@@ -25,6 +29,30 @@ const leaveClub = (clubId, userId) => ({
     type: LEAVE_CLUB,
     clubId,
     userId,
+});
+
+const addClubMessage = (clubId, userId, message) => ({
+    type: ADD_CLUB_MESSAGE,
+    clubId,
+    userId,
+    message
+});
+
+const editClubMessage = (clubId, userId, message) => ({
+    type: EDIT_CLUB_MESSAGE,
+    clubId,
+    userId,
+    message
+});
+
+const loadClubMessages = (clubId) => ({
+    type: LOAD_CLUB_MESSAGES,
+    clubId,
+});
+
+const deleteClubMessage = (messageId) => ({
+    type: DELETE_CLUB_MESSAGE,
+    messageId
 });
 
 export const thunkFetchClubs = () => async (dispatch) => {
@@ -66,8 +94,48 @@ export const thunkLeaveClub = (clubId, userId) => async (dispatch) => {
     }
 };
 
+export const thunkFetchClubMessages = (clubId) => async (dispatch) => {
+    const response = await csrfFetch(`/api/club_messages/${clubId}`);
+    if (response.ok) {
+        const messages = await response.json();
+        dispatch(loadClubMessages(clubId, messages));
+    }
+};
+
+export const thunkAddClubMessage = (clubId, messageData) => async (dispatch) => {
+    const response = await csrfFetch(`/api/club_messages/${clubId}`, {
+        method: 'POST',
+        body: JSON.stringify(messageData),
+    });
+    if (response.ok) {
+        const message = await response.json();
+        dispatch(addClubMessage(clubId, message));
+    }
+};
+
+export const thunkEditClubMessage = (messageId, messageData) => async (dispatch) => {
+    const response = await csrfFetch(`/api/club_messages/message/${messageId}`, {
+        method: 'PUT',
+        body: JSON.stringify(messageData),
+    });
+    if (response.ok) {
+        const message = await response.json();
+        dispatch(editClubMessage(message.club_id, message));
+    }
+};
+
+export const thunkDeleteClubMessage = (clubId, messageId) => async (dispatch) => {
+    const response = await csrfFetch(`/api/club_messages/message/${messageId}`, {
+        method: 'DELETE',
+    });
+    if (response.ok) {
+        dispatch(deleteClubMessage(clubId, messageId));
+    }
+};
+
 const initialState = {
     clubs: {},
+    messages: {}
 };
 
 export default function clubsReducer(state = initialState, action) {
@@ -116,6 +184,43 @@ export default function clubsReducer(state = initialState, action) {
                             (member) => member !== userId
                         ),
                     },
+                },
+            };
+        }
+        case LOAD_CLUB_MESSAGES: {
+            const newState = { ...state, messages: { ...state.messages } };
+            newState.messages[action.clubId] = action.messages;
+            return newState;
+        }
+        case ADD_CLUB_MESSAGE: {
+            const { clubId, message } = action;
+            return {
+                ...state,
+                messages: {
+                    ...state.messages,
+                    [clubId]: [...(state.messages[clubId] || []), message],
+                },
+            };
+        }
+        case EDIT_CLUB_MESSAGE: {
+            const { clubId, message } = action;
+            return {
+                ...state,
+                messages: {
+                    ...state.messages,
+                    [clubId]: state.messages[clubId].map((msg) =>
+                        msg.id === message.id ? message : msg
+                    ),
+                },
+            };
+        }
+        case DELETE_CLUB_MESSAGE: {
+            const { clubId, messageId } = action;
+            return {
+                ...state,
+                messages: {
+                    ...state.messages,
+                    [clubId]: state.messages[clubId].filter((msg) => msg.id !== messageId),
                 },
             };
         }
